ed917f399b350385ad95442b5fbaa0f20e0d2e61
Zou Lab <41780741+HuiyangYu@users.noreply.github.com>
2024-03-19 13:29:20 +0800

    Update command.sh

--- PanDepth/command.sh
+++ PanDepth/command.sh
@@ -7,12 +7,23 @@ do
         do
             for readType in SR LR
             do
-                /usr/bin/time -v sambamba depth region -c 0 -t ${thread} -L chr.bed -o sambamba.${readType}.sort.${fileType}_t${thread}_r${repeat}.txt ${readType}.sort.${fileType} 2>sambamba.${readType}.sort.${fileType}_t${thread}_r${repeat}.log
                 /usr/bin/time -v mosdepth -t ${thread} mosdepth.${readType}.sort.${fileType}_t${thread}_r8 ${readType}.sort.${fileType} 2>mosdepth.${readType}.sort.${fileType}_t${thread}_r${repeat}.log
                 /usr/bin/time -v megadepth ${readType}.sort.${fileType} --gzip --no-coverage-stdout --coverage --threads ${thread} --prefix megadepth.${readType}.sort.${fileType}_t${thread}_r${repeat} 2>megadepth.${readType}.sort.${fileType}_t${thread}_r${repeat}.log
                 /usr/bin/time -v bamtocov --skip-output -T ${thread} -o bamtocov.${readType}.sort.${fileType}_t${thread}_r8.txt ${readType}.sort.${fileType} 2>bamtocov.${readType}.sort.${fileType}_t${thread}_r${repeat}.log
                 /usr/bin/time -v pandepth -i ${readType}.sort.${fileType} -o pandepth.${readType}.sort.${fileType}_t${thread}_r${repeat} -t ${thread} 2>pandepth.${readType}.sort.${fileType}_t${thread}_r${repeat}.log
             done
+        done
+    done
+done
+
+#for sambamba
+for thread in 1 2 3 4 6 12 24
+do
+    for repeat in {1..10}
+    do
+        for readType in SR LR
+        do
+            /usr/bin/time -v sambamba depth base -c 0 -t ${thread} -o sambamba.${readType}.sort.bam_t${thread}_r${repeat}.txt ${readType}.sort.bam 2>sambamba.${readType}.sort.bam_t${thread}_r${repeat}.log
         done
     done
 done
@@ -29,3 +40,7 @@ do
         done
     done
 done
+
+
+
+
